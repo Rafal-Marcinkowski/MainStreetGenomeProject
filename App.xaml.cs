950d9d05a56deea893787173d68fe519316d137e
Rafal-Marcinkowski <rafal.marcinkowski94@gmail.com>
2024-04-24 21:38:29 +0200

    1

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,11 +2,9 @@
 using DataAccess.DBAccess;
 using Forge.OpenAI;
 using MainStreetGenomeProject.MVVM;
-using MainStreetGenomeProject.MVVM.ViewModels;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
-using ValidationComponent.DataMaintenance;
 
 namespace MainStreetGenomeProject;
 
@@ -38,11 +36,11 @@ public partial class App : Application
         {
             options.AuthenticationInfo = Configuration["ApiKey"]!;
         });
-        services.AddSingleton(provider => Configuration);
-        services.AddSingleton<ISQLDataAccess, SQLDataAccess>();
-        services.AddSingleton<ICompanyData, CompanyData>();
-        services.AddSingleton<IThreadData, ThreadData>();
-        services.AddSingleton<ICommentData, CommentData>();
+        services.AddSingleton(Configuration);
+        services.AddScoped<ISQLDataAccess, SQLDataAccess>();
+        services.AddScoped<ICompanyData, CompanyData>();
+        services.AddScoped<IThreadData, ThreadData>();
+        services.AddScoped<ICommentData, CommentData>();
         services.AddSingleton<ViewModelManager>();
     }
 }
